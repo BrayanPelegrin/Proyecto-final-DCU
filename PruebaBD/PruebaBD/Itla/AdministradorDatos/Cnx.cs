9c99817c37fcb7be62e6f9929ef5f439ee70ed56
BrayanPelegrin <Peletu109@gmail.com>
2021-04-27 12:46:56 -0400

    Clase y tablas

--- PruebaBD/PruebaBD/Itla/AdministradorDatos/Cnx.cs
+++ PruebaBD/PruebaBD/Itla/AdministradorDatos/Cnx.cs
@@ -5,6 +5,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.OleDb;
+using System.Data;
+using System.Security.Cryptography;
+using System.Windows.Forms;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
 
 namespace PruebaBD.Itla.AdministradorDatos
 {
@@ -21,12 +27,89 @@ namespace PruebaBD.Itla.AdministradorDatos
         private static List<byte[]> ListadoRostros = new List<byte[]>();
         public static int totalRostros;
 
-        public static bool GuardarImagen(string Nombres, string Apellido, string Matricula, string Contraseña, byte[] Imagen);        
+        public static bool GuardarImagen(string Nombres, string Apellido, string Matricula, byte[] Captura, string Contraseña)     
         {
             cnnx.Open();
-           OleDbCommand cmd = new OleDbCommand("INSERT INTO ESTUDIANTES (NOMBRE, APELLIDO, MATRICULA, CAPTURA, CONTRASEÑA) VALUES('"+Nombre+"','"+Apellido+"','"+Matricula+"','"+Rostros+"','"+contraseña+"';");
+
+            OleDbCommand cmd = new OleDbCommand("INSERT INTO ESTUDIANTES (NOMBRE, APELLIDO, MATRICULA, CAPTURA, CONTRASEÑA) VALUES('" + Nombre + "','" + Apellido + "','" + Matricula + "',?,'" + contraseña + "';");
+            OleDbParameter parRostros = new OleDbParameter(@"Captura", OleDbType.VarBinary, Captura.Length);
+            parRostros.Value = Rostros;
+            cmd.Parameters.Add(Rostros);
+            int Resultado = cmd.ExecuteNonQuery();
+            cnnx.Close();
+            return Convert.ToBoolean(Resultado);
+        }
+
+        public static DataTable Consultar(DataGridView DATA)
+        {
+
+            cnnx.Open();
+            OleDbCommand cmd = new OleDbCommand("SELECT * FROM ESTUDIANTES", cnnx);
+            OleDbDataAdapter da = new OleDbDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            da.Fill(dt);
+            DATA.DataSource = dt;
+            int Cont = dt.Rows.Count;
+            Apellido = new string[Cont];
+            Matricula = new string[Cont];
+            contraseña = new string[Cont];
+
+            for (int i = 0; i < Cont; i++)
+            {
+
+                Nombre[i] = dt.Rows[i]["Nombre"].ToString();
+                Apellido[i] = dt.Rows[i]["Apellido"].ToString();
+                Matricula[i] = dt.Rows[i]["Matricula"].ToString();
+                Rostros = (byte[])dt.Rows[i]["Captura"];
+                contraseña[i] = dt.Rows[i]["Contraseña"].ToString();
+                ListadoRostros.Add(Rostros);
+
+            }
+            try
+            {
+                DATA.Columns[0].Width = 60;
+                DATA.Columns[0].Width = 160;
+                DATA.Columns[0].Width = 160;
+                for (int i = 0; i < Cont; i++)
+                {
+
+                    DATA.Rows[i].Height = 110;
+                }
+            }
+            catch(Exception) 
+            {
+
+                throw;
+
+            }
+            totalRostros = Cont;
+            cnnx.Close();
+            return dt;
+        }
 
 
-        } 
+        public static byte[] ConvertImgToBinary(Image img)
+        {
+
+            Bitmap bmp = new Bitmap(img);
+            MemoryStream Memoria = new MemoryStream();
+            bmp.Save(Memoria, ImageFormat.Bmp);
+
+            byte[] Imagen = Memoria.ToArray();
+
+            return Imagen;
+        }
+
+        public static Image ConvertBinary(int c)
+        {
+
+            Image Imagen;
+            byte[] img = ListadoRostros[c];
+            MemoryStream Memoria = new MemoryStream(img);
+            Imagen = Image.FromStream(Memoria);
+            Memoria.Close();
+            return Imagen;
+
+        }
     }
 }
